ee35fdbf0265bb06820c5b2c0b41de44cad9f1fa
Mark Skinner <markskinner850@gmail.com>
2019-04-08 12:34:13 -0400

    Clean up javascript

--- js/main.js
+++ js/main.js
@@ -1,67 +1,64 @@
-$(function() {
+function displaySearchField() {
   $('#search-icon').on('click', function() {
-    //$('#search-main').addClass('d-none');
-    //$('#search-text').removeClass('d-none');
     $('#search-main').hide('slow');
     $('#search-text').show('slow');
   });
-  $("#search-clear").click(function(){
+}
+
+function clearSearchField() {
+  $('#search-clear').click(function(){
     let searchText = document.getElementById('search-item').value;
-    if (searchText === "") {
-      //$('#search-text').addClass('d-none');
+    if (searchText === '') {
       $('#search-text').hide('slow');
-      $("#search-results").hide('slow', function(){
-        $("#search-results").remove();
+      $('#search-results').hide('slow', function(){
+        $('#search-results').remove();
       });
-      //$("#search-results").remove();
-      //$('#search-main').removeClass('d-none');
       $('#search-main').show('slow');
     } else {
-       $("#search-item").val('');
-       $("#search-item").focus();
+       $('#search-item').val('');
+       $('#search-item').focus();
     }
   });
+}
+
+function submitEntry() {
   $('#submit').on('click', function() {
-    //console.log("Hello");
     $('.container').addClass('extra-height');
-    $("#search-results").remove();
-    $('#wiki-container').append("<div class='col-12' id='search-results' style='display: none;'></div>");
+    $('#search-results').remove();
+    $('#wiki-container').append('<div class="col-12" id="search-results" style="display: none;"></div>');
     let searchText = document.getElementById('search-item').value;
     let searchUrl = 'https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=' +
     searchText + '&callback=?';
-    /*$.ajax({
-      type:"GET",
-      url: searchUrl,
-      async:false,
-      dataType:"JSON",
-      success:function(data){
-        console.log(data);
-      },
-      error:function(errorMessage) {
-        alert("Error");
-      }
-    })*/
     $.getJSON(searchUrl, function(wikiData) {
-      //console.log("inside");
       for (let i = 0; i < wikiData[1].length; i++) {
         let title = wikiData[1][i];
         let description = wikiData[2][i];
-        if (description === "") {
-          description = "Description Not Available";
+        if (description === '') {
+          description = 'Description Not Available';
         }
         let link = wikiData[3][i];
-        $('#search-results').append("<a href='" + link +
-          "' target='_blank'><div class='col-12 results'><h4>" + title +
-          "</h4><p>" + description + "</p></div></a>");
+        $('#search-results').append('<a href="' + link +
+          '" target="_blank"><div class="col-12 results"><h4>' + title +
+          '</h4><p>' + description + '</p></div></a>');
       }
       $('#search-results').show('slow');
     })
   });
+}
 
-  $("#search-item").on('keyup', function (e) {
+function enterKeySubmit() {
+  $('#search-item').on('keyup', function (e) {
     if (e.keyCode === 13) {
       event.preventDefault();
       $('#submit').click();
     }
   });
-})
+}
+
+//Run on page load
+$(function() {
+  displaySearchField();
+  clearSearchField();
+  submitEntry();
+  enterKeySubmit()
+});
